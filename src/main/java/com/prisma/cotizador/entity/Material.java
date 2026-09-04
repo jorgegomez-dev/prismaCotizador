e44539a3a528805c04eaba5e67b41350d33d7ed2
jorgegomez-dev <jorgegomez.dev@gmail.com>
2026-09-04 12:32:53 -0300

    Update application.properties - Enable to connect Postgres or MySQL connection - Testing result = working

--- src/main/java/com/prisma/cotizador/entity/Material.java
+++ src/main/java/com/prisma/cotizador/entity/Material.java
@@ -1,15 +1,18 @@
 package com.prisma.cotizador.entity;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.math.BigDecimal;
 
 @Entity
+@NoArgsConstructor
+@AllArgsConstructor
 @Getter
 @Setter
-@Table(name= "material")
 public class Material {
 
     @Id
