4941f2c6199e5da85365e1b3e3d4eda799dc0c51
jorgegomez-dev <jorgegomez.dev@gmail.com>
2026-09-16 17:25:51 -0300

    Update connection to database in MySQL. Test ok

--- src/main/java/com/prisma/cotizador/CotizadorApplication.java
+++ src/main/java/com/prisma/cotizador/CotizadorApplication.java
@@ -3,6 +3,7 @@ package com.prisma.cotizador;
 import com.prisma.cotizador.entity.Material;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseFactory;
 
 import java.math.BigDecimal;
 
@@ -11,7 +12,19 @@ public class CotizadorApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CotizadorApplication.class, args);
+
+//		Material material = new Material();
+//		material.setName("MDF");
+//		material.setThicknessMM("3");
+//		material.setHeightMM(1830);
+//		material.setWidthMM(2600);
+//		material.setPricePesos(BigDecimal.valueOf(32560.50));
+//		material.setType("Wood");
+//		material.setSpeedCuttingMMS(25.0);
+//		material.setStock30x30(0);
+//		material.setStock60x30(1);
+//		material.setStock90x60(8);
+//		System.out.println(material);
 	}
-		Material mat = new Material();
 
 }
